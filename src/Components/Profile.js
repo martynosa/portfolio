b099d8b4a05df1135f65a5e4ee74de0ba2703c89
martiN <78382341+martynosa@users.noreply.github.com>
2022-11-13 18:01:58 +0200

    initial commit

---
+++ src/Components/Profile.js
@@ -0,0 +1,48 @@
+import classes from './Profile.module.css';
+
+const Profile = () => {
+  return (
+    <div className={classes['profile-container']}>
+      <nav className={classes.nav}>
+        <a
+          href="https://github.com/martynosa/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <ion-icon name="logo-github"></ion-icon>
+          Github
+        </a>
+        <a
+          href="https://www.linkedin.com/in/martynosa/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <ion-icon name="logo-linkedin"></ion-icon>
+          LinkedIn
+        </a>
+      </nav>
+
+      <div className={classes.content}>
+        <img src="/img/Profile-image-tall.jpg" alt="mugshot" />
+
+        <h1>
+          <span>M</span>artin <span>E</span>nev
+        </h1>
+
+        <p>
+          What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
+          and typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer took a galley
+          of type and scrambled it to make a type specimen book. It has survived
+          not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in
+          the 1960s with the release of Letraset sheets containing Lorem Ipsum
+          passages, and more recently with desktop publishing software like
+          Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
---
+++ src/Components/Projects.js
@@ -0,0 +1,26 @@
+import classes from './Projects.module.css';
+
+const Projects = () => {
+  return (
+    <div className={classes['projects-container']}>
+      <nav className={classes.header}></nav>
+      <div className={classes['project-container']}>
+        <img
+          className={classes.image}
+          src="/img/jayson-hinrichsen-PTXO7a35rFY-unsplash.jpg"
+          alt="project screenshot"
+        />
+      </div>
+
+      <div className={classes['project-container']}>
+        <img
+          className={classes.image}
+          src="/img/olena-sergienko-a54BiGFA9Ws-unsplash.jpg"
+          alt="project screenshot"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
