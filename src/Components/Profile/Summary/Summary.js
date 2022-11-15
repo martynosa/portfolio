9c494840b94f1aabfec6471f80913dac267b57b0
martiN <78382341+martynosa@users.noreply.github.com>
2022-11-16 00:09:36 +0200

    started working on the experience section

---
+++ src/Components/Profile/Summary/Summary.js
@@ -0,0 +1,38 @@
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
+
+import classes from './Summary.module.css';
+import Stack from './Stack';
+
+const Summary = () => {
+  const [text] = useTypewriter({
+    words: ['Martin Enev'],
+    loop: 0,
+  });
+
+  return (
+    <div className={classes.summary}>
+      <img
+        className={classes.mugshot}
+        src="/img/Profile-image-square2.jpg"
+        alt="mugshot"
+      />
+
+      <h1 className={classes.name}>
+        {text}
+        <Cursor cursorColor={'#7048e8'} />
+      </h1>
+
+      <h2 className={classes.question}>Who am I?</h2>
+      <p className={classes.answer}>
+        What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry. Lorem Ipsum has been the industry's standard
+        dummy text ever since the 1500s, when an unknown printer took a galley
+        of type and scrambled it to make a type specimen book.
+      </p>
+
+      <Stack />
+    </div>
+  );
+};
+
+export default Summary;
